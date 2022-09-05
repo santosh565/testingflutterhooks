5c0b21135362c3f71fc80aa54f2741c339a53803
hsotnas76 <hsotnas76@gmail.com>
2022-09-05 23:41:23 +0545

    step 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([E? Function(T?)? transform]) =>
+      map(transform ?? (e) => e).where((e) => e != null).cast();
+}
+
+void testIt() {
+  final values = [1, 2, null, 3];
+  // final nonNullValues = values.compactMap();
+  // debugPrint(nonNullValues.toString());
+  // final nonNullValues = values.compactMap((e) {
+  //   if (e != null && e > 10) {
+  //     return e;
+  //   } else {
+  //     return null;
+  //   }
+  // });
+}
+
 void main() {
+  testIt();
   runApp(const MyApp());
 }
 
@@ -21,6 +41,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const url =
+    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg';
+
 class MyHomePage extends HookWidget {
   const MyHomePage({
     super.key,
@@ -28,28 +51,31 @@ class MyHomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
-    );
+    // final image =
+    // useFuture(NetworkAssetBundle(Uri.parse(url))
+    //     .load(url)
+    //     .then((data) => data.buffer.asUint8List())
+    //     .then(
+    //       (data) => Image.memory(data),
+    //     ));
+
+    final future = useMemoized((() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then(
+          (data) => Image.memory(data),
+        )));
+
+    final snapShot = useFuture(future);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
       body: Column(
-        children: <Widget>[
-          TextField(
-            controller: controller,
-          ),
-          Text('You typed ${text.value}'),
-        ],
+        children: [
+          snapShot.data,
+        ].compactMap().toList(),
       ),
     );
   }
