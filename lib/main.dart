18823c3b558e20d7a516aa7611d9198d316dd210
hsotnas76 <hsotnas76@gmail.com>
2022-09-05 22:43:51 +0545

    step 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,34 +11,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+Stream<String> getTime() => Stream.periodic(
+      const Duration(seconds: 1),
+      (_) => DateTime.now().toIso8601String(),
+    );
 
-  final String title;
+class MyHomePage extends HookWidget {
+  const MyHomePage({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final dateTime = useStream(getTime());
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(dateTime.data ?? 'Home Page'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+          children: const <Widget>[],
         ),
       ),
     );
