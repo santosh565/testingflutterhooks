db338fb08ba23766a84b1ad0376bb09de58a81eb
hsotnas76 <hsotnas76@gmail.com>
2022-09-07 08:26:18 +0545

    step 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,7 @@
-import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([E? Function(T?)? transform]) =>
-      map(transform ?? (e) => e).where((e) => e != null).cast();
-}
-
-class CountDown extends ValueNotifier<int> {
-  late StreamSubscription sub;
-  CountDown({required int from}) : super(from) {
-    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
-        .takeWhile((value) => value >= 0)
-        .listen((value) {
-      this.value = value;
-    });
-  }
-
-  @override
-  void dispose() {
-    sub.cancel();
-    super.dispose();
-  }
-}
 
 void main() {
   runApp(const MyApp());
@@ -45,6 +23,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const url =
+    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg';
+const imageHeight = 300.0;
+
+extension Normalize on num {
+  num normalized(
+    num selfRangeMin,
+    num selfRangeMax, [
+    num normalizedRangeMin = 0.0,
+    num normalizedRangeMax = 1.0,
+  ]) =>
+      (normalizedRangeMax - normalizedRangeMin) *
+          ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) +
+      normalizedRangeMin;
+}
+
 class MyHomePage extends HookWidget {
   const MyHomePage({
     super.key,
@@ -52,13 +46,62 @@ class MyHomePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final countDown = useMemoized((() => CountDown(from: 20)));
-    final notifier = useListenable(countDown);
+    final opacity = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      upperBound: 1.0,
+      lowerBound: 0.0,
+    );
+    final size = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      upperBound: 1.0,
+      lowerBound: 0.0,
+    );
 
+    final controller = useScrollController();
+    useEffect(() {
+      controller.addListener(() {
+        final newOpacity = max(imageHeight - controller.offset, 0.0);
+        debugPrint(newOpacity.toString());
+        final normalized = newOpacity.normalized(0.0, imageHeight).toDouble();
+        opacity.value = normalized;
+        size.value = normalized;
+      });
+      return null;
+    }, [controller]);
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Page'),
-        ),
-        body: Text(notifier.value.toString()));
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          SizeTransition(
+            sizeFactor: size,
+            axis: Axis.vertical,
+            axisAlignment: -1.0,
+            child: FadeTransition(
+              opacity: opacity,
+              child: Image.network(
+                url,
+                height: imageHeight,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              controller: controller,
+              itemCount: 100,
+              itemBuilder: ((context, index) {
+                return ListTile(
+                  title: Text('Person ${index + 1}'),
+                );
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
